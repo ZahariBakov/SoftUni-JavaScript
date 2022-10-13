8ec4148fa38ac14015176eaa0a7a97db230416fa
ZahariBakov <zaharibakov@gmail.com>
2022-10-13 13:18:36 +0300

    Create folder "First-Step-in-Coding" Add filse "01Hello.js", "02PrintNumber.js", "03SquareArea/js", "04InchesToCentimeters.js" and "solve.js"

---
+++ First-Step-in-Coding-Lab-JS/04InchesToCentimeters.js
@@ -0,0 +1,8 @@
+function inchToCentimeters(input){
+    let inch = Number(input[0])
+    let result = inch * 2.54
+    console.log(result)
+}
+
+inchToCentimeters(["7"])
+inchToCentimeters(["5"])
---
+++ First-Step-in-Coding-Lab-JS/solve.js
@@ -0,0 +1,9 @@
+function solve(){
+    let arr = ["Gosho", "Pesho", "Ivan", "Todor"];
+
+    console.table(arr);
+    console.log(arr[2]);
+    console.log(arr.length);
+}
+
+solve()
